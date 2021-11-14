42927a38a7beaf02c79a62b7ed8ab0181c5a91e2
Jaroslav <zheglanov3@gmail.com>
2021-11-14 20:21:18 +0300

    arrays to string fixed

--- src/com/tms/lesson3/task1.java
+++ src/com/tms/lesson3/task1.java
@@ -1,5 +1,7 @@
 package com.tms.lesson3;
 
+import java.util.Arrays;
+
 public class task1 {
     public static void main(String[] args) {
         int[] a= new int[10];
@@ -8,8 +10,6 @@ public class task1 {
         for (int b = 2;b < a.length; b++) {
             a[b] = a[b - 1]+a [b - 2];
         }
-        for (int c = 0;c < a.length; c++) {
-            System.out.println(a[c]);
-        }
+        System.out.println(Arrays.toString(a));
     }
 }
