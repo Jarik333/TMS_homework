ebda336227fe313b9fb1b98b05b2a36039944851
Jaroslav <zheglanov3@gmail.com>
2021-11-16 17:47:54 +0300

    task 4 from lesson 4 is done

---
+++ src/com/tms/lesson4/Mathods.java
@@ -0,0 +1,17 @@
+package com.tms.lesson4;
+
+public class Mathods {
+    public static void main(String[] args) {
+        int a = 10;
+        String name = "jack";
+        Print(a);
+        Print(name);
+    }
+    public static void Print(int a){
+        System.out.println(a);
+    }
+    public static void Print(String b){
+        System.out.println(b);
+    }
+
+}
